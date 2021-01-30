5b6436211736aa12737166ba35c7d01808025146
Santy Santydany Dany <santydany@rays-MacBook-Air.local>
2021-01-30 13:32:28 -0500

    Final adds

---
+++ js/main.js
@@ -0,0 +1,87 @@
+(() => {
+	console.log('fired!');
+
+
+
+        let Lotion = document.querySelector ("#Lotion");
+        
+        function logMyId (){
+          console.log(this.id);
+        }
+
+        lotion.addEventlistener ("click", logMyId);
+
+
+        let Bag = document.querySelector ("#Bag");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        bag.addEventlistener ("click", logMyId);
+
+
+        let Comb = document.querySelector ("#Comb");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        comb.addEventlistener ("click", logMyId);
+
+
+        let Lipstick = document.querySelector ("#Lipstick");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        lipstick.addEventlistener ("click", logMyId);
+
+        let mascara = document.querySelector ("#mascara");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        mascara.addEventlistener ("click", logMyId);
+
+        let Mirror = document.querySelector ("#Mirror");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        mirror.addEventlistener ("click", logMyId);
+
+        let Moisturaizer = document.querySelector ("#Moisturaizer");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        moisturaizer.addEventlistener ("click", logMyId);
+
+        let nail = document.querySelector ("#nail");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        nail.addEventlistener ("click", logMyId);
+
+        let Scissors = document.querySelector ("#Scissors");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        scissors.addEventlistener ("click", logMyId);
+
+        let Soap = document.querySelector ("#Soap");
+        function logMyId ()
+        {
+          console.log(this.id);
+        }
+        soap.addEventlistener ("click", logMyId);
+
+    })()
+
+
+
+
+
+     
+    
